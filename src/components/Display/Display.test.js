37526bc02b8e4c2980b4c9f62ae1f3d89c9bb6a5
EmilyDSarani <emily.sarani@gmail.com>
2021-11-30 20:02:32 -0600

    3/3 tests done. I know Ill need to finish out Picker and Character, but they are all done for now

--- src/components/Display/Display.test.js
+++ src/components/Display/Display.test.js
@@ -1,4 +1,9 @@
 import {screen, render} from '@testing-library/react';
 import Display from './Display'
 
-it('should render the Display')
+it('should render the Display', () => {
+    const {container} = render (
+    <Display catchphrase={['holy hotwheels batman', 'let it goooooo', 'the lion sleeps toniiiight']} />);
+
+    expect(container).toMatchSnapshot();
+});
